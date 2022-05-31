5bd06ab18e44b8c6a7c8690a8bb4036a9d70a5ff
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-05-31 16:02:19 +0400

    create project

---
+++ src/test/java/tests/DemowebshopTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DemowebshopTests {
+    @Test
+    void addToCardTest(){
+        given();
+    }
+}
